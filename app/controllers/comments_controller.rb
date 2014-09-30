716e61a9eda48dd79bbc3921ec639b96781e7e24
Jenna Tauro <jennatauro@gmail.com>
2014-09-30 00:56:38 -0400

    Added ability to delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,13 @@ class CommentsController < ApplicationController
     @comment = @team.comments.create(comment_params)
     redirect_to team_path(@team)
   end
+
+  def destroy
+    @team = Team.find(params[:team_id])
+    @comment = @team.comments.find(params[:id])
+    @comment.destroy
+    redirect_to team_path(@team)
+  end
  
   private
     def comment_params
